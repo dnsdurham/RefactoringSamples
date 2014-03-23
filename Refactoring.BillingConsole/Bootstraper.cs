1989fd0b39bddfaa4bbbac61922d6663869b35d2
Ian Cottingham <icotting>
2014-03-22 22:11:41 -0500

    Added a unit of work to manage transactional resources
    Added a unit of work and a generic Entity Framework entity repository to
    serve as the object being managed by the unit of work

--- Refactoring.BillingConsole/Bootstraper.cs
+++ Refactoring.BillingConsole/Bootstraper.cs
@@ -3,8 +3,10 @@ using Microsoft.Practices.Unity;
 using Refactoring.Engines;
 using Refactoring.Managers;
 using Refactoring.ResourceAccessors;
+using Refactoring.Transactions;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +26,12 @@ namespace Refactoring.BillingConsole
             container.RegisterType<IFrequentRenterEngine, FrequentRenterEngine>();
             container.RegisterType<IPricingEngine, PricingEngine>();
             container.RegisterType<ICustomerAccessor, CustomerAccessor>();
+
+            container.RegisterType(typeof(DbContext), typeof(ClientDataContext));
+
+            container.RegisterInstance(new ClientDataContextAdapter(container.Resolve<DbContext>()), new HierarchicalLifetimeManager());
+            container.RegisterType<IDbSetFactory>(new InjectionFactory(con => con.Resolve<ClientDataContextAdapter>()));
+            container.RegisterType<IDbContext>(new InjectionFactory(con => con.Resolve<ClientDataContextAdapter>()));
 
             UnityServiceLocator locator = new UnityServiceLocator(container);
             ServiceLocator.SetLocatorProvider(() => locator);
